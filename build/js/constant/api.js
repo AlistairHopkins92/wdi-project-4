bf737423f5a225ff4cdaa19c2bb5b33d1ed69c3f
AlistairHopkins92 <alistairhopkins92@gmail.com>
2016-08-08 01:32:01 +0100

    still trying to fix it

--- build/js/constant/api.js
+++ build/js/constant/api.js
@@ -1,3 +1,7 @@
 angular
   .module('dtg')
-  .constant('API', 'http://down-to-go.herokuapp.com/api');
+  .constant('API', 'http://down-to-go.herokuapp.com/api')
+  .config(mainRouter)
+  .config(function($httpProvider) {
+    $httpProvider.interceptors.push("authInterceptor");
+  });
