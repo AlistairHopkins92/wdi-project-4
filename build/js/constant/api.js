9ebaca674bce5256ecfa4c48b58e35fbd4327ce2
AlistairHopkins92 <alistairhopkins92@gmail.com>
2016-08-08 01:51:58 +0100

    undoing some of the damage done

--- build/js/constant/api.js
+++ build/js/constant/api.js
@@ -1,7 +1,3 @@
 angular
   .module('dtg')
-  .constant('API', 'http://down-to-go.herokuapp.com/api')
-  .config(mainRouter)
-  .config(function($httpProvider) {
-    $httpProvider.interceptors.push("authInterceptor");
-  });
+  .constant('API', 'http://down-to-go.herokuapp.com/api');
