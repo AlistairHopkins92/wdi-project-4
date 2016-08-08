118799fe0b0496db4490aeb7e7772f4d7eb52e90
AlistairHopkins92 <alistairhopkins92@gmail.com>
2016-08-08 01:20:04 +0100

    realised my API is only really set up for local hosting

--- build/js/constant/api.js
+++ build/js/constant/api.js
@@ -1,3 +1,4 @@
 angular
   .module('dtg')
-  .constant('API', 'http://localhost:3000/api');
+  // .constant('API', 'http://localhost:3000/api');
+  .constant('API', 'http://down-to-go.herokuapp.com/#/api');
