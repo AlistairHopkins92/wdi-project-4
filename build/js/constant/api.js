cbf1f84ad19d5c2934eeb3000e321c616cef5f7c
AlistairHopkins92 <alistairhopkins92@gmail.com>
2016-08-08 01:28:34 +0100

    still having problems with registering and the api

--- build/js/constant/api.js
+++ build/js/constant/api.js
@@ -1,4 +1,3 @@
 angular
   .module('dtg')
-  // .constant('API', 'http://localhost:3000/api');
-  .constant('API', 'http://down-to-go.herokuapp.com/#/api');
+  .constant('API', 'http://down-to-go.herokuapp.com/api');
